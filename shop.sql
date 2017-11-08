7778e196645a42f71375b8853ec85229b5c9b894
Manphil <ywh1476747380@163.com>
2017-11-08 20:35:45 +0800

    添加数据库表的sql文件

---
+++ shop.sql
@@ -0,0 +1,467 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2017-11-08 12:29:50
+-- 服务器版本： 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `activity`
+--
+
+CREATE TABLE `activity` (
+  `activityId` int(12) NOT NULL,
+  `activityName` varchar(50) NOT NULL,
+  `activityDes` varchar(500) NOT NULL,
+  `discount` float DEFAULT '1',
+  `fullPrice` int(12) DEFAULT NULL,
+  `reducePrice` int(12) DEFAULT NULL,
+  `fullNum` int(12) DEFAULT NULL,
+  `reduceNum` int(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `address`
+--
+
+CREATE TABLE `address` (
+  `addressID` int(12) NOT NULL,
+  `userId` int(12) NOT NULL,
+  `province` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `county` varchar(50) NOT NULL,
+  `detailAddr` varchar(50) NOT NULL,
+  `conName` varchar(50) NOT NULL,
+  `conTel` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin`
+--
+
+CREATE TABLE `admin` (
+  `adminId` int(12) NOT NULL,
+  `adminName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `category`
+--
+
+CREATE TABLE `category` (
+  `cateId` int(12) NOT NULL,
+  `cateName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `chat`
+--
+
+CREATE TABLE `chat` (
+  `chatId` int(20) NOT NULL,
+  `sendUser` int(12) NOT NULL,
+  `receiveUser` int(12) NOT NULL,
+  `MsgContent` varchar(255) NOT NULL,
+  `MsgTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `collection`
+--
+
+CREATE TABLE `collection` (
+  `userId` int(12) NOT NULL,
+  `goodsId` int(12) NOT NULL,
+  `collectTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `comment`
+--
+
+CREATE TABLE `comment` (
+  `commentId` int(12) NOT NULL,
+  `userId` int(12) NOT NULL,
+  `goodsId` int(12) NOT NULL,
+  `point` int(8) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `commentTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `deliver`
+--
+
+CREATE TABLE `deliver` (
+  `deliverId` int(12) NOT NULL,
+  `orderId` int(12) NOT NULL,
+  `sendId` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `goods`
+--
+
+CREATE TABLE `goods` (
+  `goodsId` int(12) NOT NULL,
+  `goodsName` varchar(50) NOT NULL,
+  `price` int(12) NOT NULL,
+  `num` int(12) NOT NULL,
+  `upTime` datetime NOT NULL,
+  `category` int(12) NOT NULL,
+  `detailCate` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `activityId` int(12) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `imagepath`
+--
+
+CREATE TABLE `imagepath` (
+  `pathId` int(12) NOT NULL,
+  `goodId` int(12) NOT NULL,
+  `path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `indent`
+--
+
+CREATE TABLE `indent` (
+  `orderId` int(12) NOT NULL,
+  `userId` int(12) NOT NULL,
+  `orderTime` datetime NOT NULL,
+  `oldPrice` float NOT NULL,
+  `newPrice` float NOT NULL,
+  `isPay` tinyint(1) NOT NULL,
+  `isSend` tinyint(1) NOT NULL,
+  `isReceive` tinyint(1) NOT NULL,
+  `isComplete` tinyint(1) NOT NULL,
+  `addressId` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `itemId` int(12) NOT NULL,
+  `orderId` int(12) NOT NULL,
+  `goodsId` int(12) NOT NULL,
+  `num` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `shopcart`
+--
+
+CREATE TABLE `shopcart` (
+  `userId` int(12) NOT NULL,
+  `goodsid` int(12) NOT NULL,
+  `cateDate` datetime NOT NULL,
+  `goodsNum` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(12) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `regTime` datetime NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`activityId`);
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`addressID`),
+  ADD KEY `addressID` (`addressID`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adminId`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cateId`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatId`),
+  ADD KEY `sendUser` (`sendUser`),
+  ADD KEY `receiveUser` (`receiveUser`);
+
+--
+-- Indexes for table `collection`
+--
+ALTER TABLE `collection`
+  ADD PRIMARY KEY (`userId`,`goodsId`),
+  ADD KEY `collection_ibfk_2` (`goodsId`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`commentId`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `goodsId` (`goodsId`);
+
+--
+-- Indexes for table `deliver`
+--
+ALTER TABLE `deliver`
+  ADD PRIMARY KEY (`deliverId`),
+  ADD KEY `orderId` (`orderId`);
+
+--
+-- Indexes for table `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`goodsId`),
+  ADD KEY `activityId` (`activityId`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indexes for table `imagepath`
+--
+ALTER TABLE `imagepath`
+  ADD PRIMARY KEY (`pathId`),
+  ADD KEY `goodid` (`goodId`);
+
+--
+-- Indexes for table `indent`
+--
+ALTER TABLE `indent`
+  ADD PRIMARY KEY (`orderId`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `orderGoods` (`orderTime`),
+  ADD KEY `addressId` (`addressId`);
+
+--
+-- Indexes for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`itemId`),
+  ADD KEY `orderId` (`orderId`),
+  ADD KEY `goodsId` (`goodsId`);
+
+--
+-- Indexes for table `shopcart`
+--
+ALTER TABLE `shopcart`
+  ADD PRIMARY KEY (`userId`,`goodsid`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `goodsid` (`goodsid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `activityId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- 使用表AUTO_INCREMENT `address`
+--
+ALTER TABLE `address`
+  MODIFY `addressID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- 使用表AUTO_INCREMENT `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adminId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- 使用表AUTO_INCREMENT `category`
+--
+ALTER TABLE `category`
+  MODIFY `cateId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- 使用表AUTO_INCREMENT `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+--
+-- 使用表AUTO_INCREMENT `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `commentId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- 使用表AUTO_INCREMENT `deliver`
+--
+ALTER TABLE `deliver`
+  MODIFY `deliverId` int(12) NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `goodsId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+--
+-- 使用表AUTO_INCREMENT `imagepath`
+--
+ALTER TABLE `imagepath`
+  MODIFY `pathId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+--
+-- 使用表AUTO_INCREMENT `indent`
+--
+ALTER TABLE `indent`
+  MODIFY `orderId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- 使用表AUTO_INCREMENT `orderitem`
+--
+ALTER TABLE `orderitem`
+  MODIFY `itemId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `userId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`sendUser`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`receiveUser`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `collection`
+--
+ALTER TABLE `collection`
+  ADD CONSTRAINT `collection_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `collection_ibfk_2` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`goodsId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`goodsId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `deliver`
+--
+ALTER TABLE `deliver`
+  ADD CONSTRAINT `deliver_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `indent` (`orderId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `goods`
+--
+ALTER TABLE `goods`
+  ADD CONSTRAINT `goods_ibfk_1` FOREIGN KEY (`activityId`) REFERENCES `activity` (`activityId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `goods_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`cateId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `imagepath`
+--
+ALTER TABLE `imagepath`
+  ADD CONSTRAINT `imagepath_ibfk_1` FOREIGN KEY (`goodId`) REFERENCES `goods` (`goodsId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `indent`
+--
+ALTER TABLE `indent`
+  ADD CONSTRAINT `indent_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `indent_ibfk_2` FOREIGN KEY (`addressId`) REFERENCES `address` (`addressID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD CONSTRAINT `orderitem_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `indent` (`orderId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderitem_ibfk_2` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`goodsId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `shopcart`
+--
+ALTER TABLE `shopcart`
+  ADD CONSTRAINT `shopcart_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `shopcart_ibfk_2` FOREIGN KEY (`goodsid`) REFERENCES `goods` (`goodsId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
